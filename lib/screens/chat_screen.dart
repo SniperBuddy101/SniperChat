2e45c36d94f637dba9da6a4dd85d6a4fc4d22aa0
shreyashkarnik <karnik.shreyash@gmail.com>
2020-06-07 23:34:05 +0530

    SniperChat by Shreyash Karnik Removed sign out button from the Chat Screen.

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -61,15 +61,6 @@ class _ChatScreenState extends State<ChatScreen> {
 
     return Scaffold(
       appBar: AppBar(
-        leading: null,
-        actions: <Widget>[
-          IconButton(
-              icon: Icon(Icons.close),
-              onPressed: () async {
-                await _auth.signOut();
-                Navigator.pop(context);
-              }),
-        ],
         title: Text('️Chat with ${widget.person2UserName}'),
       ),
       body: SafeArea(
